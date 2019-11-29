a8f7b1135d58c06e1aa4974f7b78b8bded8e8d8c
Sudarshan Senthil Kumar <senthilsudarshan@gmail.com>
2019-11-30 01:48:23 +0530

    Added all programs

--- p6-prims.py
+++ p6-prims.py
@@ -5,9 +5,10 @@ class Graph():
                     for row in range(vertices)]
 
     def printMST(self, parent):
-        print "Edge \tWeight"
+        print("Edge \tWeight")
         for i in range(1, self.V):
-            print parent[i], "-", i, "\t", self.graph[i][ parent[i] ]
+            print (parent[i], "-", i, "\t", self.graph[i][ parent[i] ])
+            
     def minKey(self, key, mstSet):
         min = 999
         for v in range(self.V):
